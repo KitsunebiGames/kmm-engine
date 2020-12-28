eb237ce05a2c462347231d4db941230d3d1e452e
Member1221 <cli-luna@protonmail.com>
2020-12-29 00:27:37 +0100

    WIP text selection and text editing

--- source/engine/input/text.d
+++ source/engine/input/text.d
@@ -2,7 +2,7 @@ module engine.input.text;
 import engine;
 import bindbc.sdl;
 import std.string;
-import std.utf : strideBack;
+import std.utf : strideBack, stride;
 
 /**
     Text input system
@@ -14,6 +14,8 @@ private static:
 package(engine) static:
     void update(SDL_Event* event) {
 
+        import std.stdio : writeln;
+
         // Skip when we're not reading text
         if (!isReadingText) return;
 
@@ -21,16 +23,70 @@ package(engine) static:
         switch(event.type) {
             // Handle clipboard and delete
             case SDL_EventType.SDL_KEYDOWN:
-                if (event.key.keysym.sym == SDL_Keycode.SDLK_RETURN && returnEndsInput && composition.length == 0) {
+                if (event.key.keysym.sym == SDL_Keycode.SDLK_RETURN && returnEndsInput && !isComposing) {
                     
                     // We'll end the text input here
                     endTextInput();
                     break;
 
-                } if (event.key.keysym.sym == SDL_Keycode.SDLK_BACKSPACE && text.length > 0) {
+                } else if (event.key.keysym.sym == SDL_Keycode.SDLK_BACKSPACE && text.length > 0) {
 
-                    // Remove a character
-                    text.length -= strideBack(text);
+                    if (selection == 0) {
+
+                        // Get amount of bytes in the UTF8 character
+                        uint charWidth = strideBack(text, cursor);
+
+                        // Remove it then update the cursor
+                        text = text[0..cursor-charWidth] ~ text[cursor+selection..$]; // Remove a character
+                        cursor = cursor-charWidth;
+
+                    } else {
+
+                        // In case of selection remove multiple
+                        // In this case the cursor should stay in the same place but the selection be reset
+                        text = text[0..cursor] ~ text[cursor+selection..$];
+                        selection = 0;
+                    }
+
+                } else if (event.key.keysym.sym == SDL_Keycode.SDLK_LEFT && text.length > 0 && (event.key.keysym.mod == SDL_Keymod.KMOD_LSHIFT || event.key.keysym.mod == SDL_Keymod.KMOD_RSHIFT)) {
+
+                    // TODO: allow backwards selection by shifting cursor?
+
+                    // Skip if our selection is already 0
+                    if (selection == 0) break;
+
+                    // Move the selection by 1 UTF8 codepoint
+                    selection -= strideBack(text, cursor+selection);
+
+                } else if (event.key.keysym.sym == SDL_Keycode.SDLK_RIGHT && text.length > 0 && (event.key.keysym.mod == SDL_Keymod.KMOD_LSHIFT || event.key.keysym.mod == SDL_Keymod.KMOD_RSHIFT)) {
+
+                    // Skip if we're already at end of string
+                    if (cursor+selection == text.length) break;
+
+                    // Move the selection by 1 UTF8 codepoint
+                    selection += stride(text, cursor+selection);
+
+                } else if (event.key.keysym.sym == SDL_Keycode.SDLK_LEFT && text.length > 0) {
+
+                    // Skip if we're already at beginning of string
+                    if (cursor == 0) break;
+
+                    // Move the cursor by 1 UTF8 codepoint
+                    cursor -= strideBack(text, cursor);
+
+                    // Moving cursor will reset selection
+                    selection = 0;
+
+                } else if (event.key.keysym.sym == SDL_Keycode.SDLK_RIGHT && text.length > 0) {
+
+                    // Skip if we're already at end of string
+                    if (cursor == text.length) break;
+
+                    // Move the cursor by 1 UTF8 codepoint
+                    cursor += stride(text, cursor);
+
+                    // Moving cursor will reset selection
+                    selection = 0;
 
                 } else if (event.key.keysym.sym == SDL_Keycode.SDLK_c && (event.key.keysym.mod == SDL_Keymod.KMOD_LCTRL || event.key.keysym.mod == SDL_Keymod.KMOD_RCTRL)) {
                     
@@ -39,12 +95,15 @@ package(engine) static:
 
                 } else if (event.key.keysym.sym == SDL_Keycode.SDLK_v && (event.key.keysym.mod == SDL_Keymod.KMOD_LCTRL || event.key.keysym.mod == SDL_Keymod.KMOD_RCTRL)) {
                     
-                    // Skip if there's no text to paste
+                    // Skip if there's no text to paste, otherwise fetch text from clipboard
                     if (!SDL_HasClipboardText()) break;
+                    string cText = cast(string)SDL_GetClipboardText().fromStringz;
 
-                    // Paste text
-                    text ~= cast(string)SDL_GetClipboardText().fromStringz;
+                    // Paste text, replacing any selected text in the process
+                    text = text[0..cursor] ~ cText ~ text[cursor+selection..$];
 
+                    // Move cursor by the amount of unicode points in text
+                    cursor += cText.length;
                 }
                 break;
             
@@ -57,7 +116,15 @@ package(engine) static:
 
             // Handle input
             case SDL_EventType.SDL_TEXTINPUT:
-                text ~= composition.ptr.fromStringz;
+                // Get the composed text
+                string composedText = composition.ptr.fromStringz;
+
+                // Replace selection if need be
+                text = text[0..cursor] ~ composedText ~ text[cursor+selection..$];
+                cursor += stride(composedText);
+
+                // Reset selection after this.
+                selection = 0;
                 break;
 
             // We only wanna handle textevents
@@ -74,6 +141,16 @@ package(engine) static:
     }
 
 public static:
+
+    /**
+        Where the cursor is in the text
+    */
+    uint cursor;
+
+    /**
+        The characters selected from beginning of cursor
+    */
+    uint selection;
 
     /**
         Where the cursor is in the composition
@@ -142,6 +219,8 @@ public static:
         // Reset candidate and text
         cselection = 0;
         ccursor = 0;
+        cursor = 0;
+        selection = 0;
         composition = "";
 
         return outText;
