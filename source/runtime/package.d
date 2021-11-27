7f3c7e73453f402c037fceb034e235e4fe5e1d3a
LunaTheFoxgirl <cli-luna@protonmail.com>
2021-11-27 21:01:33 +0100

    Rename game to runtime

---
+++ source/runtime/package.d
@@ -0,0 +1,38 @@
+module runtime;
+import engine;
+
+/**
+    Initialize game
+*/
+void _init() {
+    GameWindow.setSwapInterval(SwapInterval.VSync);
+    GameWindow.title = "Kitsunemimi Runtime (No Game Loaded)";
+}
+
+/**
+    Update game
+*/
+void _update() {
+
+}
+
+/**
+    Game cleanup
+*/
+void _cleanup() {
+
+}
+
+/**
+    Render border
+*/
+void _border() {
+
+}
+
+/**
+    Post-update
+*/
+void _postUpdate() {
+
+}
