c7e93fd91cd7102f4c2060863635f130b2e430d2
LunaTheFoxgirl <cli-luna@protonmail.com>
2021-11-28 21:38:51 +0100

    gct imports

---
+++ source/engine/gct/package.d
@@ -0,0 +1,13 @@
+/*
+    Game Content
+
+    Copyright © 2020, Luna Nielsen
+    Distributed under the 2-Clause BSD License, see LICENSE file.
+    
+    Authors: Luna Nielsen
+*/
+module engine.gct;
+public import engine.gct.gameobject;
+public import engine.gct.room;
+public import engine.gct.tile;
+public import engine.gct.states;
